1c31a230579de44cfd076b072ebe02dfa7a81e0a
Jay Skrobola <jay.skrobola@infor.com>
2020-08-10 13:50:06 -0400

    Fixed error in setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
         name='TestPackage',
-        version-'0.1',
+        version='0.1',
         description='Test package',
         packages=['TestPackage'])
